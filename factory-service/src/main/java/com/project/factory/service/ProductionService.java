38b9dc7659a110d03b859a086b316abe5ecb00d0
ICAROCAETANO <icaro.caetano@gmail.com>
2026-06-17 23:26:46 -0300

    feat: add new module

--- factory-service/src/main/java/com/project/factory/service/ProductionService.java
+++ factory-service/src/main/java/com/project/factory/service/ProductionService.java
@@ -4,11 +4,7 @@ public class ProductionService {
 
     public void execute() {
 
-        var product =
-                new Product(
-                        "001",
-                        "Notebook"
-                );
+        var product = new Product("001", "Notebook");
 
         System.out.println(product);
     }
