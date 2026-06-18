c9bdd92cfc4e43973de0678ae7aecb8291c7faa8
ICAROCAETANO <icaro.caetano@gmail.com>
2026-06-17 22:55:28 -0300

    first commit

---
+++ src/main/java/com/project/poc_own_module_factory/PocOwnModuleFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.project.poc_own_module_factory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocOwnModuleFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocOwnModuleFactoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/poc_own_module_factory/PocOwnModuleFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.poc_own_module_factory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocOwnModuleFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
